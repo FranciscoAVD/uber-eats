299796298b4975966a257f3c67daa8914a22a00e
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-10-08 13:55:51 -0400

    updated styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,15 +7,16 @@ import { cn } from "@/lib/utils"
 const nav = [
     { label: "Home", href:"/", icon: HomeIcon },
     { label: "Services", href:"/services", icon: GridIcon },
-    { label: "Activity", href:"activity", icon: TicketIcon },
-    { label: "Account", href:"account", icon: UserIcon },
+    { label: "Activity", href:"/activity", icon: TicketIcon },
+    { label: "Account", href:"/account", icon: UserIcon },
 ]
 export default function Footer({ className }: { className?: string }) {
     const active = usePathname();
     return (
-        <footer className={cn(`flex items-center justify-between pt-2 pb-4 border-t-2  ${className}`)}>
+        <footer className={cn(`flex items-center justify-evenly pt-4 pb-6 border-t-2  ${className}`)}>
             {nav.map(n => (
-                <Button 
+                <Button
+                size={"icon"}
                 variant={"ghost"} 
                 className={`flex-col h-fit text-xs ${active===n.href ? "text-neutral-900" : "text-neutral-500"}`}
                 asChild>
