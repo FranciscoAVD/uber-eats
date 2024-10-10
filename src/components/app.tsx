8ebbc2f45ac3e114e4d38ed6dfaafbaa7c17f7f9
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-10-10 06:41:39 -0400

    updated footer styling

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,17 +3,24 @@ import Footer from './footer'
 import Header from './header'
 import Address from './eats/delivery/address'
 import { cn } from '@/lib/utils'
+import DeliverOptions from './eats/delivery/deliver-options'
+import Cart from './eats/cart/cart'
 
-export default function App({className}:{className?: string}) {
+export default function App({ className }: { className?: string }) {
   return (
-    <div className={cn(`relative flex flex-col h-full ${className}`)}>
-        <Header />
-        <main className='grow px-4 pt-2'>
-          <div>
-            <Address />
+    <div className={cn(`flex flex-col h-full ${className}`)}>
+      <Header />
+      <main className='relative grow px-4 pt-2'>
+        <div className='flex items-center justify-between'>
+          <Address />
+          <div className='flex items-center'>
+            <DeliverOptions />
+            <Cart />
           </div>
-        </main>
-        <Footer className='mt-auto'/>
+
+        </div>
+      </main>
+      <Footer className='fixed bottom-0 inset-x-0 bg-white z-50' />
     </div>
   )
 }
